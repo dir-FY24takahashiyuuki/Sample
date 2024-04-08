f2806f62d7c3a02f5a36fc77372c6bd4b01c8cc8
dir-FY24takahashiyuuki <yuki.takahashi.b138@dirbato.co.jp>
2024-04-08 14:28:48 +0900

    最初のコミット

---
+++ Sample/src/sample/ifSample.java
@@ -0,0 +1,15 @@
+package sample;
+
+public class ifSample {
+
+	public static void main(String[] args) {
+		var score = 80; // 成績
+		
+		 if (score >= 60) {
+			 System.out.println("合格です！");
+		 } else {
+			 System.out.println("赤点です。。。");
+		 }
+
+}
+}
